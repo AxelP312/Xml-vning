8eff27ec41803b552158b8d9f049f429e408c6e3
AxelP312 <axepetaxe@gmail.com>
2020-09-23 11:08:29 +0200

    Nya saker med dezeralirstinsering

--- XmlÖVning/Program.cs
+++ XmlÖVning/Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Dynamic;
+﻿using System.IO;
+using System.Dynamic;
 using System;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace XmlÖVning
 {
@@ -7,16 +10,38 @@ namespace XmlÖVning
     {
         static void Main(string[] args)
         {   
-            spelaren player1 = new spelaren();
+            // spelaren player1 = new spelaren();
 
-            System.Console.WriteLine("What is your name?");
-            player1.name = Console.ReadLine();
+            // System.Console.WriteLine("What is your name?");
+            // player1.name = Console.ReadLine();
             
-            System.Console.WriteLine("What is your main dish?");
-            player1.dish = Console.ReadLine();
+            // System.Console.WriteLine("What is your main dish?");
+            // player1.dish = Console.ReadLine();
 
-            System.Console.WriteLine("What is the description of the dish?");
-            player1.description = Console.ReadLine();
+            // System.Console.WriteLine("What is the description of the dish?");
+            // player1.description = Console.ReadLine();
+            
+
+            
+            XmlSerializer SerializerTest = new XmlSerializer(typeof(spelaren));
+
+            FileStream file = File.Open("SparadData.xml", FileMode.OpenOrCreate);
+
+            // SerializerTest.Serialize(file, player1);
+            
+            spelaren player1 = (spelaren) SerializerTest.Deserialize(file);
+
+            file.Close();
+            
+            Console.WriteLine(player1.name);
+            Console.ReadLine();
+            
+
+
+            
+            
+
+
         }
     }
 }
